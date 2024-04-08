9f26bd236782024100ac319ffe71c485ffb0177e
yiranshu <shuyiran1425@hotmail.com>
2024-04-07 21:54:27 -0400

    destructuring

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,20 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* render ProjectItem components here */}
+    {projects.map((project) =>(
+        <div key={project.id}>
+          <p>{project.name}</p> 
+          <p>{project.about}</p> 
+        {/* {project.technologies.map((technology)=>( <span key={technology}>{technology}</span>
+          )
+        )} */}
+        <span> {project.technologies} </span>
+         
+        </div>
+        ))}
+        </div>
     </div>
   );
 }
